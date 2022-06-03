c815f798799ede29963b48fee2fd50cdf61f0a73
Jason Foord <jf@tag.com.au>
2022-06-03 10:27:46 +0930

    remove unused parts

--- plugin.lua
+++ plugin.lua
@@ -20,45 +20,10 @@ function GetPages(props)
     return pages
 end
 
--- Optional function to define model if plugin supports more than one model
-function GetModel(props)
-    local model = {}
-    --[[ #include "src/model.lua" ]]
-    return model
-end
-
 -- Define User configurable Properties of the plugin
 function GetProperties()
     local props = {}
-    --[[ #include "src/properties.lua" ]]
     return props
-end
-
--- Optional function to define pins on the plugin that are not connected to a Control
-function GetPins(props)
-    local pins = {}
-    --[[ #include "src/pins.lua" ]]
-    return pins
-end
-
--- Optional function to update available properties when properties are altered by the user
-function RectifyProperties(props)
-    --[[ #include "src/rectify_properties.lua" ]]
-    return props
-end
-
--- Optional function to define components used within the plugin
-function GetComponents(props)
-    local components = {}
-    --[[ #include "src/components.lua" ]]
-    return components
-end
-
--- Optional function to define wiring of components used within the plugin
-function GetWiring(props)
-    local wiring = {}
-    --[[ #include "src/wiring.lua" ]]
-    return wiring
 end
 
 -- Defines the Controls used within the plugin
--- src/components.lua
+++
@@ -1,9 +0,0 @@
-table.insert(components,{
-  Name = "main_mixer",
-  Type = "mixer",
-  Properties =   
-  {
-    ["n_inputs"] = 8,
-    ["n_outputs"] = 1,
-  }
-})
